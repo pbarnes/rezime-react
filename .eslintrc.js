eef26358e875827cef4681e6b18c9f43d07fc685
Phil Barnes <phillip.barnes@gmail.com>
2022-06-12 16:05:36 -0400

    add eslint, prettier, associated configs

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends":[
+        "eslint:recommended",
+        'plugin:@typescript-eslint/recommended',
+        "plugin:react/recommended",
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        '@typescript-eslint/eslint-plugin',
+        'prettier',
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
